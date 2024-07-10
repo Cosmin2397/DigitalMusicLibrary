c66cc18609a7a1241c9dcc884d3d007daf7cf258
Cosminmoisa97 <Moisa.Io.Cosmin@student.utcluj.ro>
2024-07-10 17:31:15 +0300

    Add project files.

---
+++ DigitalMusicLibrary/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DigitalMusicLibrary.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ArtistId { get; set; }
+
+        [NotMapped]
+        public List<Song> Songs { get; set; }
+    }
+}
---
+++ DigitalMusicLibrary/Models/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalMusicLibrary.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Length { get; set; }
+
+        public int AlbumId { get; set; }
+    }
+}
