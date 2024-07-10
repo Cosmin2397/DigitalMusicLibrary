0ef5c6c5eb2146be0c65b0acbf3eef3b2a765a7c
Cosminmoisa97 <Moisa.Io.Cosmin@student.utcluj.ro>
2024-07-10 18:57:31 +0300

    Add IArtistService interface and ArtistService class

---
+++ DigitalMusicLibrary/Services/ArtistService.cs
@@ -0,0 +1,32 @@
+﻿using DigitalMusicLibrary.Models;
+
+namespace DigitalMusicLibrary.Services
+{
+    public class ArtistService : IArtistService
+    {
+        private readonly
+        public ArtistService()
+        {
+            
+        }
+        public Task<bool> DeleteArtistAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Artist>> GetArtists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Artist>> SearchAsync(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateArtistAsync(int id, Artist artist)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DigitalMusicLibrary/Services/IArtistService.cs
@@ -0,0 +1,15 @@
+﻿using DigitalMusicLibrary.Models;
+
+namespace DigitalMusicLibrary.Services
+{
+    public interface IArtistService
+    {
+        Task<IEnumerable<Artist>> GetArtists();
+
+        Task<List<Artist>> SearchAsync(string searchTerm);
+
+        Task<bool> UpdateArtistAsync(int id, Artist artist);
+
+        Task<bool> DeleteArtistAsync(int id);
+    }
+}
