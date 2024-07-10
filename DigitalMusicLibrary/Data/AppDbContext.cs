c230b31bbaecb4838ee08faad18950f6a3d902ef
Cosminmoisa97 <Moisa.Io.Cosmin@student.utcluj.ro>
2024-07-10 17:40:23 +0300

    Add Entity Framework Core and Db model context

---
+++ DigitalMusicLibrary/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using DigitalMusicLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DigitalMusicLibrary.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
